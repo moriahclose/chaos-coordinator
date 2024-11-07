74d00d1ed04d78aa44fe2e59acdbce122b97e215
moriahclose <moriah.v.close@gmail.com>
2024-11-07 00:42:18 -0500

    Hide stuff that should be hidden

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import 'dotenv/config'
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6gswsL3lGz6pEKmSs-Z89msrCnUhfF58",
-  authDomain: "chaos-coordinator.firebaseapp.com",
-  projectId: "chaos-coordinator",
-  storageBucket: "chaos-coordinator.firebasestorage.app",
-  messagingSenderId: "560553628890",
-  appId: "1:560553628890:web:a09ae1e988d600fac841e3",
-  measurementId: "G-XVLTFG22YH"
+  apiKey: procces.env.FIRESTORE_API_KEY,
+  authDomain: procces.env.FIREBASE_AUTH_DOMAIN,
+  projectId: procces.env.FIREBASE_PROJECT_ID,
+  storageBucket: procces.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: procces.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: procces.env.FIREBASE_APP_ID,
+  measurementId: procces.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
