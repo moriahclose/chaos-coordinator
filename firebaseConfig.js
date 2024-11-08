7f0b5f21d5439e7ec181b61a1fc6c537225a146d
moriahclose <moriah.v.close@gmail.com>
2024-11-08 00:58:39 -0500

    Fix process variable errors

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import 'dotenv/config'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,18 +9,18 @@ import 'dotenv/config'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: procces.env.FIRESTORE_API_KEY,
-  authDomain: procces.env.FIREBASE_AUTH_DOMAIN,
-  projectId: procces.env.FIREBASE_PROJECT_ID,
-  storageBucket: procces.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: procces.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: procces.env.FIREBASE_APP_ID,
-  measurementId: procces.env.FIREBASE_MEASUREMENT_ID
-};
+    apiKey: process.env.FIRESTORE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // For Firestore
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app) // For Firestore
 // const analytics = getAnalytics(app);
 
 export { db }
